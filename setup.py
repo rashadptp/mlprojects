72442df83e1167af061996634b8a904edf8365cd
rashadptp <ptprashad@gmail.com>
2024-02-21 00:55:42 +0530

    EDA and Problem Statement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from typing import List
 HYPEN_E_DOT = "-e ."
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the requirements
+    this function will return  the list of requirements
     '''
     requirements = []
     with open(file_path) as file_obj:
